d29bfafc1550fba9ea99bd994522747920e6560c
WZW12315 <1058309476@qq.com>
2025-06-27 20:15:03 +0800

    这是版本一

---
+++ pnc_map.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "pnc_map.h"
+
+using namespace std;
+
+void PNC_Map::mapInfo() {
+    cout << "This is PNC_Map class" << endl;    
+    cout << "This is a test" << endl;
+}
---
+++ process.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "process.h"
+
+using namespace std;
+void Process::planProcess()
+{
+    cout << "Plan process" << endl;
+    my_map.mapInfo();
+    cout << "planning suceessful!" << endl;
+}
