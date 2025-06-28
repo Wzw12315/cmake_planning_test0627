9859c4a0b5a73ec34e002cfe483776a08b3bc63f
WZW12315 <1058309476@qq.com>
2025-06-28 21:40:31 +0800

    to: 修改planning_main.cpp中的结束输出信息
    在主函数中，将原有的 "planning end!" 输出信息注释掉，添加了两个新的输出信息："planning end1!" 和 "planning end2!"。这些修改是为了更详细的记录规划过程的结束，并区分不同的结束状态。

--- planning_main.cpp
+++ planning_main.cpp
@@ -8,6 +8,8 @@ int main()
      cout << "planning start!" << endl;
     Process pro;
     pro.planProcess();
-    cout << "planning end!" << endl;
+    // cout << "planning end!" << endl;
+    cout << "planning end1!" << endl;
+    cout << "planning end2!" << endl;
     return 0;
 }
